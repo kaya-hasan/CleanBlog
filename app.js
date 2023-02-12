5b973694dab66c7fa631fb679ea36574c9bf7e9a
Hasan Kaya <hasankaya3546@hotmail.com>
2023-02-12 22:33:39 +0300

    Clean Blog Updates

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda baslatildi`);
+  console.log(`Sunucu ${port} portunda baslatildi...`);
 });
