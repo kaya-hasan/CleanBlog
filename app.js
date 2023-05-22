6fc8564b57d819d718db4f103f37adcff68345c8
Hasan Kaya <hasankaya3546@hotmail.com>
2023-05-23 01:14:38 +0300

    ejs files created and uploaded

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 
+//TEMPLATES ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
 app.use(express.static('public'));
 
-//const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
